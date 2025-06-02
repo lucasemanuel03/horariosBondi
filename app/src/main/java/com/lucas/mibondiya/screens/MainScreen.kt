235cf0a6167143e81923cfe2a368e49a0ecc6570
lucasemanuel03 <leblucas3@gmail.com>
2025-06-02 15:52:13 -0300

    modificando screen principal con cards

--- app/src/main/java/com/lucas/mibondiya/screens/MainScreen.kt
+++ app/src/main/java/com/lucas/mibondiya/screens/MainScreen.kt
@@ -13,14 +13,20 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.Build
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ElevatedCard
@@ -31,21 +37,29 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.lucas.mibondiya.R
 import com.lucas.mibondiya.navigation.AppScreens
+import com.lucas.mibondiya.service.MockDataService
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -57,7 +71,7 @@ fun MainScreen(navController: NavController){
             modifier =
                 Modifier
                     .fillMaxSize()
-                    .background(color = MaterialTheme.colorScheme.background),
+                    ,
             topBar = {
                 CenterAlignedTopAppBar(
                     title = { Text("Horarios Interurbanos") },
@@ -86,17 +100,14 @@ fun MainScreen(navController: NavController){
 
 @Composable
 fun ContenidoPrincipal(modifier: Modifier = Modifier, padding: PaddingValues, navController: NavController){
+    val options = listOf<String>("Jesús Maria a Córdoba", "Córdoba a Jesús María")
+
     Box(
         modifier = Modifier
             .fillMaxSize()
+
     ) {
-        // Fondo con imagen
-        Image(
-            painter = painterResource(id = R.drawable.fondo),
-            contentDescription = null,
-            contentScale = ContentScale.Crop,
-            modifier = Modifier.matchParentSize()
-        )
+
 
         Column(
             modifier = modifier
@@ -108,73 +119,54 @@ fun ContenidoPrincipal(modifier: Modifier = Modifier, padding: PaddingValues, na
 
             ) {
 
-            InputDestinoRadio(navController)
-            Spacer(modifier = Modifier.height(16.dp))
-            GoToEditHorariosCard(navController)
+            ElevatedCardWithImage(
+                title = "A Córdoba",
+                label = "Ver Horarios",
+                onClickFun = {navController.navigate(AppScreens.HorariosNowScreen.route + "/${options[0]}")},
+                idImage = R.drawable.cordoba_image,
+                backgroundColor = MaterialTheme.colorScheme.surfaceBright)
+            Spacer(modifier = Modifier.height(12.dp))
+            ElevatedCardWithImage(
+                title = "A Jesús María",
+                label = "Ver Horarios",
+                onClickFun = {navController.navigate(AppScreens.HorariosNowScreen.route + "/${options[1]}")},
+                idImage = R.drawable.jesus_maria,
+                backgroundColor = MaterialTheme.colorScheme.surfaceBright)
+
+            Spacer(modifier = Modifier.height(26.dp))
+            ElevatedCardWithIcon(
+                title = "Gestionar Horarios",
+                label = "Panel de Gestiones",
+                onClickFun = {navController.navigate(AppScreens.ShowHorariosForEditScreen.route)},
+                idIcon = Icons.Default.Build,
+                backgroundColor = MaterialTheme.colorScheme.secondaryContainer)
+
+            Spacer(modifier = Modifier.height(12.dp))
+            ElevatedCardWithIcon(
+                title = "Nuevo Horario",
+                label = "Panel de Horarios",
+                onClickFun = {navController.navigate(AppScreens.AddHorario.route)},
+                idIcon = Icons.Default.AddCircle,
+                backgroundColor = MaterialTheme.colorScheme.secondaryContainer)
         }
+
     }
 }
 
+
+
 @Composable
-fun InputDestinoRadio(navController: NavController,modifier: Modifier = Modifier){
-    val radioOptions = listOf<String>("Jesús Maria a Córdoba", "Córdoba a Jesús María")
-    val (selectedOption, onOptionSelected) = remember { mutableStateOf(radioOptions[0]) }
-    ElevatedCard(
-        modifier = Modifier.padding(26.dp),
-        elevation = CardDefaults.cardElevation(
-            defaultElevation = 16.dp
-        ),
-    ) {
-        Column(
-            modifier
-                .selectableGroup()
-                .background(color = MaterialTheme.colorScheme.surfaceContainer)
-                .padding(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
+fun GoToEditHorariosCard(navController: NavController){
 
-            Text(
-                text = "Elige el Destino",
-
-                fontWeight = FontWeight.Bold,
-                style = MaterialTheme.typography.titleLarge,
-                modifier = Modifier.padding(bottom = 24.dp)
-            )
-
-            // Radio buttons
-            radioOptions.forEach { text ->
-                Row(
-                    Modifier
-                        .fillMaxWidth()
-                        .height(56.dp)
-                        .selectable(
-                            selected = (text == selectedOption),
-                            onClick = { onOptionSelected(text) },
-                            role = Role.RadioButton
-                        )
-                        .padding(horizontal = 16.dp),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.Center
-                ) {
-                    RadioButton(
-                        selected = (text == selectedOption),
-                        onClick = null // null recommended for accessibility with screen readers
-                    )
-                    Text(
-                        text = text,
-                        style = MaterialTheme.typography.bodyLarge,
-                        modifier = Modifier.padding(start = 9.dp)
-                    )
-                }
-            }
-
-            Spacer(modifier = Modifier.height(16.dp))
+    ElevatedCard() {
+        Column {
+            Text(text = "Editar Horarios")
 
             Button(
-                onClick = { navController.navigate(route = AppScreens.HorariosNowScreen.route + "/$selectedOption") }
+                onClick = { navController.navigate(route = AppScreens.ShowHorariosForEditScreen.route) }
             ) {
                 Icon(
-                    imageVector = Icons.Default.Search,
+                    imageVector = Icons.Default.Edit,
                     contentDescription = null,
                     modifier = Modifier.size(ButtonDefaults.IconSize)
 
@@ -183,33 +175,146 @@ fun InputDestinoRadio(navController: NavController,modifier: Modifier = Modifier
                 Text(
                     fontWeight = FontWeight.Bold,
                     color = MaterialTheme.colorScheme.onPrimary,
-                    text = "Buscar")
+                    text = "Ir a Edicion")
+            }
+
+            Button(
+                onClick = { navController.navigate(route = AppScreens.ShowHorariosForEditScreen.route) }
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = null,
+                    modifier = Modifier.size(ButtonDefaults.IconSize)
+
+                )
+                Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+                Text(
+                    fontWeight = FontWeight.Bold,
+                    color = MaterialTheme.colorScheme.onPrimary,
+                    text = "Ir a Edicion")
             }
         }
     }
+
 }
 
-
+@Preview
 @Composable
-fun GoToEditHorariosCard(navController: NavController){
+fun ElevatedCardWithIcon(title: String = "Gestionar Horarios",
+                         label: String = "Panel de Gestiones",
+                         onClickFun: () -> Unit = {},
+                         idIcon: ImageVector = Icons.Default.Info,
+                         backgroundColor: Color = MaterialTheme.colorScheme.surfaceContainerLow,
+                         colorTitle: Color = MaterialTheme.colorScheme.onSurface
+                          )
+{
 
-    ElevatedCard() {
-        Button(
-            onClick = { navController.navigate(route = AppScreens.ShowHorariosForEditScreen.route) }
-        ) {
+    ElevatedCard(
+        onClick = {onClickFun()},
+
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 6.dp
+        ),
+        modifier = Modifier
+            .size(width = 350.dp, height = 80.dp),
+        shape = MaterialTheme.shapes.medium,
+        colors = CardDefaults.cardColors(containerColor = backgroundColor),
+
+    ) {
+        Row(modifier = Modifier.padding(16.dp).fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+            ){
+
+            Column(
+
+                verticalArrangement = Arrangement.Center
+            ) {
+
+                Text(
+                    style = MaterialTheme.typography.titleSmall,
+                    text = label,
+                )
+
+                Text(
+                    text = title,
+                    color = colorTitle,
+                    style = MaterialTheme.typography.headlineSmall,
+
+
+                )
+            }
+            Spacer(modifier = Modifier.width(16.dp))
+
             Icon(
-                imageVector = Icons.Default.Edit,
-                contentDescription = null,
-                modifier = Modifier.size(ButtonDefaults.IconSize)
-
+                imageVector = idIcon,
+                contentDescription = "descr",
+                modifier = Modifier.size(40.dp)
             )
-            Spacer(Modifier.size(ButtonDefaults.IconSpacing))
-            Text(
-                fontWeight = FontWeight.Bold,
-                color = MaterialTheme.colorScheme.onPrimary,
-                text = "Ir a Edicion")
+
         }
+
     }
-
-
 }
+
+@Preview
+@Composable
+fun ElevatedCardWithImage(title: String = "A Córdoba",
+                          label: String = "Ver Horarios",
+                          onClickFun: () -> Unit = {},
+                          idImage: Int = R.drawable.cordoba_image,
+                          backgroundColor: Color = Color(0xFFFFFFFF),
+
+)
+{
+
+    ElevatedCard(
+        onClick = {onClickFun()},
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 6.dp
+        ),
+        modifier = Modifier
+            .size(width = 350.dp, height = 90.dp),
+        shape = MaterialTheme.shapes.medium,
+        colors = CardDefaults.cardColors(containerColor = backgroundColor),
+
+        ) {
+        Row(modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ){
+
+            Column(
+                modifier = Modifier.padding(16.dp),
+                verticalArrangement = Arrangement.Center
+            ) {
+
+                Text(
+                    style = MaterialTheme.typography.titleSmall,
+                    text = label,
+                )
+
+                Text(
+                    text = title,
+                    style = MaterialTheme.typography.headlineMedium,
+                    fontWeight = FontWeight.Bold,
+
+                    )
+            }
+            Spacer(modifier = Modifier.width(16.dp))
+
+            Image(
+                painter = painterResource(id = idImage),
+                contentDescription = "Descriptive text for the image",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .height(100.dp)
+                    .width(120.dp)
+            )
+
+        }
+
+    }
+}
+
+
